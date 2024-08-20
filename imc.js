b067a572f932d2a642da7f4e75e6a0c7b8089c30
KauanFelipe007 <160232860+KauanFelipe007@users.noreply.github.com>
2024-08-20 08:15:19 -0300

    v,gukl

--- imc.js
+++ imc.js
@@ -13,17 +13,17 @@ function imc () {
         let classificacao = '';
 
         if (valorIMC < 18.5){
-            classificacao = 'abaixo do peso.';
+            classificacao = 'abaixo de media ideal.';
         }else if (valorIMC < 25) {
-            classificacao = 'com peso ideal. Parabéns!!!';
+            classificacao = 'com peso ideal . Parabéns!!!';
         }else if (valorIMC < 30){
-            classificacao = 'levemente acima do peso.';
+            classificacao = 'um pouco acima do peso, deveria fazer uma dieta.';
         }else if (valorIMC < 35){
-            classificacao = 'com obesidade grau I.';
+            classificacao = 'com obesidade não muito seria.';
         }else if (valorIMC < 40){
-            classificacao = 'com obesidade grau II';
+            classificacao = 'com obesidade grau II, preocupante procure um nutricionistas';
         }else {
-            classificacao = 'com obesidade grau III. Cuidado!!';
+            classificacao = 'com obesidade grau III. Cuidado vc pode ter problemas graves!!';
         }
 
         resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
